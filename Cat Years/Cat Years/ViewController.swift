c500961811dbc4fb22b02bfd68bb7700d9c50e77
Jim Veneskey <github@strifecrafter.com>
2015-08-25 12:31:05 -0400

    Creating the Cat Age Calculator

---
+++ Cat Years/Cat Years/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Cat Years
+//
+//  Created by jim Veneskey on 8/25/15.
+//  Copyright © 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var catAgeTextField: UITextField!
+  
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findAge(sender: AnyObject) {
+    
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
