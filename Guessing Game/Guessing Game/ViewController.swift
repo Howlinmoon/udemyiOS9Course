3cd4287897cc2697a5b9da364658380439763159
Jim Veneskey <github@strifecrafter.com>
2015-08-27 12:35:32 -0400

    Game is working, but cant hide unhide the play again button

--- Guessing Game/Guessing Game/ViewController.swift
+++ Guessing Game/Guessing Game/ViewController.swift
@@ -10,17 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomNumber = Int(arc4random_uniform(10)+1)
+    
     @IBOutlet weak var guessField: UITextField!
     
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func checkGuess(sender: AnyObject) {
+        // When this button is pressed, we need to retrieve the number input by the user
+        // and compare it to our randomly generated randomNumber
+        var resultText = ""
+        let inputNumber = Int(guessField.text!)
+        if inputNumber == randomNumber {
+            resultText = "CORRECT"
+            //playAgain.hidden = false
+        } else if inputNumber < randomNumber {
+            resultText = "LOW"
+        } else {
+            resultText = "HIGH"
+        }
+        
+        // Update our result label
+        resultLabel.text = resultText
+        
+    }
     
+    
+    @IBAction func playAgain(sender: AnyObject) {
+        // if this button is enabled, clicking it should clear out the old guess
+        // clear out the old result
+        // and randomize up a new number
+        randomNumber = Int(arc4random_uniform(10)+1)
+        resultLabel.text = ""
+        guessField.text = ""
+        // and then, hide itself again
+        //playAgain.hidden = true
+        
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // hide the play again button
+        //playAgain.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
