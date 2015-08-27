1848927f831e33e31e64d0db06df88e8af61e1fc
Jim Veneskey <github@strifecrafter.com>
2015-08-27 12:18:37 -0400

    Attempting a guess the random number game completely from scratch

---
+++ Guessing Game/Guessing Game/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Guessing Game
+//
+//  Created by jim Veneskey on 8/27/15.
+//  Copyright © 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var guessField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func checkGuess(sender: AnyObject) {
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
