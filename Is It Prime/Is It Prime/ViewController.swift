56336bf33af586f2f3c161750abbea5a6ce4758a
Howlinmoon <github@strifecrafter.com>
2015-08-29 21:03:58 -0400

    Tying in the prime determination code

--- Is It Prime/Is It Prime/ViewController.swift
+++ Is It Prime/Is It Prime/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    @IBAction func isitPrime(sender: AnyObject) {
+        
+        let number = Int(numberTextField.text!)!
+        
+        var isPrime = true
+        
+        if number == 1 {
+            isPrime = true
+        } else if number == 2 {
+            isPrime = false
+        } else if number > 2 {
+            
+            for var i = 2; i < number; i++ {
+                
+                if number % i == 0 {
+                    print("This number is NOT a prime - \(i) is a factor")
+                    isPrime = false
+                }
+            }
+        }
+        print(isPrime)
+        
+    }
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
