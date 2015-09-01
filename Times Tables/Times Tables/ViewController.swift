711677e90b47dfef9446689be64adc13752d85f8
Jim Veneskey <github@strifecrafter.com>
2015-09-01 06:57:44 -0400

    Finished the slider times table program, but still have issues with constraints for some reason

--- Times Tables/Times Tables/ViewController.swift
+++ Times Tables/Times Tables/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var table: UITableView!
+    
+    // Holds the actual value of the slider
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    // Called when the user adjusts the slider
+    @IBAction func sliderMoved(sender: AnyObject) {
+        // reload the table when the slider is moved
+        table.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +30,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+
+    // we only want the first 20 rows in the table
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        
+        let timesTable = Int(sliderValue.value * 20)
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "\((indexPath.row + 1) * timesTable)"
+        
+        return cell
+    }
+
 
 
 }
