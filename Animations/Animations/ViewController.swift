95dcd7eeabc5bd9dd014a38041a43c7a162ac1fd
Howlinmoon <github@strifecrafter.com>
2015-09-06 08:22:18 -0400

    manual animation of alien completed

--- Animations/Animations/ViewController.swift
+++ Animations/Animations/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var frameNumber = 1
+    
+    @IBOutlet weak var animationPicture: UIImageView!
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        // increment the frame number
+        frameNumber += 1
+        if frameNumber > 5 {
+            frameNumber = 1
+        }
+        let frameName = "frame\(frameNumber).png"
+        animationPicture.image = UIImage(named: frameName)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
