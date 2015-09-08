aa2cb7322056510bca81b2a3849373229fa64646
Jim Veneskey <github@strifecrafter.com>
2015-09-08 12:26:48 -0400

    Downtown Akron

--- Map Demo/Map Demo/ViewController.swift
+++ Map Demo/Map Demo/ViewController.swift
@@ -16,7 +16,24 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // downtown Akron
+        var latitude:CLLocationDegrees = 41.0838776
+        var longitude: CLLocationDegrees = -81.5187311
+        
+        // Zoom level
+        var latDelta: CLLocationDegrees = 0.01
+        var lonDelta: CLLocationDegrees = 0.01
+        
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        var location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region, animated: true)
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
