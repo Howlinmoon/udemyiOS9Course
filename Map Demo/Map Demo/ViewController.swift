c292875bb41fe160e14678464edb65cab126316b
Jim Veneskey <github@strifecrafter.com>
2015-09-09 07:18:36 -0400

    Map function is not working - others have the same issue

--- Map Demo/Map Demo/ViewController.swift
+++ Map Demo/Map Demo/ViewController.swift
@@ -8,14 +8,27 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet var map: MKMapView!
     
+
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // request authorization when app is in use, no background stalking
+        locationManager.requestWhenInUseAuthorization()
+        // start updating the users location
+        locationManager.startUpdatingLocation()
+        
+        
+        
+        
 
         // downtown Akron
         let latitude:CLLocationDegrees = 41.0838776
@@ -42,7 +55,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
         map.addAnnotation(annotation)
         
         // UI Long Press Gesture Recognizer, trailing colon required to pass details to the named function 'action'
-        var uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
         
         // how long does the user have to press to recognize this?
         uilpgr.minimumPressDuration = 2
@@ -57,7 +70,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
         print("You performed a long press")
         
         // co-ordinates relative to the map in the view
-        var touchPoint = gestureRecognizer.locationInView(self.map)
+        let touchPoint = gestureRecognizer.locationInView(self.map)
         
         let newCoordinate: CLLocationCoordinate2D = map.convertPoint(touchPoint, toCoordinateFromView:self.map)
         let annotation = MKPointAnnotation()
@@ -71,6 +84,27 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
     }
     
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+        let userLocation:CLLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        
+        // Zoom level
+        let latDelta: CLLocationDegrees = 0.01
+        let lonDelta: CLLocationDegrees = 0.01
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        self.map.setRegion(region, animated: true)
+        
+    }
     
 
     override func didReceiveMemoryWarning() {
