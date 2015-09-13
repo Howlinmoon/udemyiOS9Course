c332c63ef57b1765b10f3f95702546b53f65a3e2
Howlinmoon <github@strifecrafter.com>
2015-09-12 22:32:34 -0400

    Starting an audio project

---
+++ Working With Audio/Working With Audio/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Working With Audio
+//
+//  Created by Jim Veneskey on 9/12/15.
+//  Copyright © 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let audioPath = NSBundle.mainBundle().pathForResource("bach", ofType: "mp3")!
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
