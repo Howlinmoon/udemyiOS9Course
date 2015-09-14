b98b3614e466c14b413e233529507bc506e3acb7
Jim Veneskey <github@strifecrafter.com>
2015-09-14 12:33:20 -0400

    Done with UI setup - now starting code

--- Back To Bach/Back To Bach/ViewController.swift
+++ Back To Bach/Back To Bach/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func play(sender: AnyObject) {
+    
+    }
+    
+    @IBOutlet var volumeSlider: UISlider!
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+    
+    }
+    
+    @IBOutlet var scrubSlider: UISlider!
+    
+    
+    @IBAction func scrub(sender: AnyObject) {
+    
+    }
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
