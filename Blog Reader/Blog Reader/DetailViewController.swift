3784405941aa6daf1d033250abf23bb2685cba50
Howlinmoon <github@strifecrafter.com>
2015-09-18 18:31:44 -0400

    Putting course on hold temporarily

--- Blog Reader/Blog Reader/DetailViewController.swift
+++ Blog Reader/Blog Reader/DetailViewController.swift
@@ -43,3 +43,36 @@ class DetailViewController: UIViewController {
 
 }
 
+
+/*
+
+BLOG STUFF
+
+https://www.googleapis.com/blogger/v3/blogs/byurl?url=http://bifftech.blogspot.com/&key=AIzaSyBWVDC6NHZX7P6GRtdPrUWpECEixmAX6NQ
+
+
+
+{
+"kind": "blogger#blog",
+"id": "6889056965376595624",
+"name": "“We're lost, but we're making good time”",
+"description": "One of many classic Yogi Berra quotes.",
+"published": "2007-05-01T11:25:08-04:00",
+"updated": "2015-09-17T02:53:51-04:00",
+"url": "http://bifftech.blogspot.com/",
+"selfLink": "https://www.googleapis.com/blogger/v3/blogs/6889056965376595624",
+"posts": {
+"totalItems": 34,
+"selfLink": "https://www.googleapis.com/blogger/v3/blogs/6889056965376595624/posts"
+},
+"pages": {
+"totalItems": 0,
+"selfLink": "https://www.googleapis.com/blogger/v3/blogs/6889056965376595624/pages"
+},
+"locale": {
+"language": "en",
+"country": "US",
+"variant": ""
+}
+}
+*/
