ae08c3da47e71dfeeb6e66cd7908748f5be8315f
Jim Veneskey <github@strifecrafter.com>
2015-10-15 06:43:34 -0400

    Starting a new Parse Project - the Instagram Clone

--- ParseStarterProject/ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ParseStarterProject/ViewController.swift
@@ -1,138 +1,18 @@
 //
 //  ViewController.swift
 //
-//  Copyright 2011-present Parse Inc. All rights reserved.
-//
+//  Instagram Clone
+//  2015/10/15 Jim Veneskey
 
 import UIKit
 import Parse
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController {
 
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
-    
-    
-    @IBAction func pause(sender: AnyObject) {
-    
-        activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
-        activityIndicator.center = self.view.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
-        view.addSubview(activityIndicator)
-        activityIndicator.startAnimating()
-        //UIApplication.sharedApplication().beginIgnoringInteractionEvents()
-    }
-    
-    
-    @IBAction func restore(sender: AnyObject) {
-    
-        activityIndicator.stopAnimating()
-        //UIApplication.sharedApplication().endIgnoringInteractionEvents()
-    }
-    
-    
-    @IBAction func createAlert(sender: AnyObject) {
-        let alert = UIAlertController(title: "Hey, There!", message: "Are you sure?", preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
-            
-            self.dismissViewControllerAnimated(true, completion: nil)
-        }))
-        
-        self.presentViewController(alert, animated: true, completion: nil)
-    }
-    
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
-        print("Image Selected")
-        // get rid of the UI Image Picker Controller
-        self.dismissViewControllerAnimated(true, completion: nil)
-        importedImage.image = image
-        
-    }
-    
-    
-    @IBAction func importImage(sender: AnyObject) {
-        
-        let image = UIImagePickerController()
-        image.delegate = self
-        // in real device use: UIImagePickerControllerSourceType.Camera
-        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-        image.allowsEditing = false
-        
-        self.presentViewController(image, animated: true, completion: nil)
-    
-    }
-    
-    
-    @IBOutlet weak var importedImage: UIImageView!
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        
-        
-        
-        
-        /*
-        
-        let operation = "UPDATE"
-        
-        if operation == "STORE" {
-        
-            // Storing the Data
-            let product = PFObject(className: "Products")
-            product["name"] = "Pumpkin Pie"
-            product["description"] = "Traditional Fall Treat"
-            product["price"] = 4.99
-            product.saveInBackgroundWithBlock { (success, error) -> Void in
-                if success == true {
-                    print("Success saving product with ID: \(product.objectId)")
-                
-                } else {
-                    print("Failed saving product")
-                    print(error)
-                }
-            }
-        } else if operation == "RETRIEVE" {
-            let query = PFQuery(className: "Products")
-            query.getObjectInBackgroundWithId("KAdsZaAGwx", block: { (object: PFObject?, error: NSError?) -> Void in
-                if error != nil {
-                    print(error)
-                } else if let product = object {
-                    
-                    let productName = product.objectForKey("name")
-                    print("The name is: \(productName)")
-                    
-                    
-                    let description = product.objectForKey("description")
-                    print("The description is: \(description)")
-                    
-                    let price = product.objectForKey("price")
-                    print("The price is: \(price)")
-                }
-            })
-        } else if operation == "UPDATE" {
-            let query = PFQuery(className: "Products")
-            query.getObjectInBackgroundWithId("KAdsZaAGwx", block: { (object: PFObject?, error: NSError?) -> Void in
-                if error != nil {
-                    print(error)
-                } else if let product = object {
-                    
-                    product["name"] = "Fruit Cake"
-                    product["description"] = "Dreaded Holiday Treat"
-                    product["price"] = 999.99
-                    
-                    product.saveInBackground()
-                    
-                }
-            })
-        }
-        
-        */
-        
     }
 
     override func didReceiveMemoryWarning() {
