3d508c50d126e92e74b4cc8d67406f2d552b4df3
Jim Veneskey <github@strifecrafter.com>
2016-03-16 12:44:54 -0400

    Starting a Flappy Bird clone project

---
+++ Flappy Bird/Flappy Bird/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  Flappy Bird
+//
+//  Created by jim Veneskey on 3/16/16.
+//  Copyright (c) 2016 Jim Veneskey. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # udemyiOS9Course
 
 Left off here:
-https://www.udemy.com/the-complete-ios-9-developer-course/learn/#/lecture/3194794
+https://www.udemy.com/the-complete-ios-9-developer-course/learn/#/lecture/3194838
+
+Skipped the Tinder clone and moved on to the Sprite Kit tutorial
+
 
 
 
